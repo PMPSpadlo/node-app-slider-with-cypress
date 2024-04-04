ed5c6b23e266d232fc746d8ed97374d01afc7829
pmpspadlo <pmp.spadlo@gmail.com>
2024-04-04 20:38:59 +0200

    -new e2e test

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -1,18 +1,34 @@
 describe('Swiper Gallery Test', function () {
-  it('Checks if second slide contains "United Kingdom"', function () {
-    cy.visit('http://localhost:3000');
-    cy.get('.swiper-button-next').click();
-    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
-  });
+    it('Checks if second slide contains "United Kingdom"', function () {
+        cy.visit('http://localhost:3000');
+        cy.get('.swiper-button-next').click();
+        cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+    });
 });
 
 describe('Swiper Gallery Test', function () {
-  it('Checks if third slide contains "Paris"', function () {
-    cy.visit('http://localhost:3000');
-    cy.get('.swiper-button-next').click();
-    cy.wait(2000);
-    cy.get('.swiper-button-next').click({ force: true });
-    cy.wait(2000);
-    cy.get('.swiper-slide-active').should('contain', 'Paris');
-  });
+    it('Checks if third slide contains "Paris"', function () {
+        cy.visit('http://localhost:3000');
+        cy.get('.swiper-button-next').click();
+        cy.wait(2000);
+        cy.get('.swiper-button-next').click({force: true});
+        cy.wait(2000);
+        cy.get('.swiper-slide-active').should('contain', 'Paris');
+    });
+});
+describe('Check all gallery functionality', function () {
+    it('Checks if main gallery container is visible', function () {
+        cy.visit('http://localhost:3000');
+        cy.get('.swiper-wrapper')
+            .should('be.visible');
+        cy.get('.swiper-slide')
+            .should('be.visible')
+            .and('have.length', 3);
+        cy.get('.swiper-button-prev')
+            .should('be.visible')
+            .click();
+        cy.get('.swiper-button-next')
+            .should('be.visible')
+            .click();
+    });
 });
